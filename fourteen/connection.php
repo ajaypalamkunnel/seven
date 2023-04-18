fe405d319f1d53ef334136ad3d3d7f5ae0a03c6b
Ajay ps <ajaypalamkunnel45@gmail.com>
2023-04-18 12:59:53 +0530

    fourteen project database created and connected succsessfully

---
+++ fourteen/connection.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+        $servername = "localhost";
+        $username = "root";
+        $password ="";      
+        $db = "productshop";
+
+        $conn = mysqli_connect($servername,$username,$password,$db);
+
+        if($conn)
+        {
+            //echo "Conected succesfully";
+        }
+        else
+        {
+            die("connection failed").mysqli_connect_error();
+        }
+    ?>
+</body>
+</html>
